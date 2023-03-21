1f148b334930721df93870458b2f13d44e43d993
jiemo499 <113921400+jiemo499@users.noreply.github.com>
2023-03-21 14:32:06 +0000

    Update App.java

--- app/src/main/java/com/github/tvbox/osc/base/App.java
+++ app/src/main/java/com/github/tvbox/osc/base/App.java
@@ -59,16 +59,18 @@ public class App extends MultiDexApplication {
         if (!Hawk.contains(HawkConfig.PLAY_TYPE)) {
             Hawk.put(HawkConfig.PLAY_TYPE, 1);
         }
-        //自定义默认配置，硬解，安全dns，缩略图
-        if (!Hawk.contains(HawkConfig.IJK_CODEC)) {
-            Hawk.put(HawkConfig.IJK_CODEC, "硬解码");
-        }
-        if (!Hawk.contains(HawkConfig.DOH_URL)) {
-            Hawk.put(HawkConfig.DOH_URL, 2);
-        }
-        if (!Hawk.contains(HawkConfig.SEARCH_VIEW)) {
-            Hawk.put(HawkConfig.SEARCH_VIEW, 2);
-        }
+        //自定义默认配置，硬解，安全dns，缩略图      
+private void initParams() {
+
+    putDefault(HawkConfig.HOME_REC, 2);       // Home Rec 0=豆瓣, 1=推荐, 2=历史
+    putDefault(HawkConfig.PLAY_TYPE, 1);      // Player   0=系统, 1=IJK, 2=Exo
+    putDefault(HawkConfig.IJK_CODEC, "硬解码");// IJK Render 软解码, 硬解码
+    putDefault(HawkConfig.HOME_SHOW_SOURCE, true);  // true=Show, false=Not show
+    putDefault(HawkConfig.HOME_NUM, 2);       // History Number
+    putDefault(HawkConfig.DOH_URL, 2);        // DNS
+    putDefault(HawkConfig.SEARCH_VIEW, 2);    // Text or Picture
+
+  }
     }
 
     public static App getInstance() {
