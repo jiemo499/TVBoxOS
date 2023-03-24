a6f6ae2bdef6818129f047a4876f193c359e9707
jiemo499 <113921400+jiemo499@users.noreply.github.com>
2023-03-24 15:15:05 +0000

    Update RemoteServer.java

--- app/src/main/java/com/github/tvbox/osc/server/RemoteServer.java
+++ app/src/main/java/com/github/tvbox/osc/server/RemoteServer.java
@@ -75,42 +75,6 @@ public class RemoteServer extends NanoHTTPD {
     private void addPostRequestProcess() {
         postRequestList.add(new InputRequestProcess(this));
     }
-
-String getpath() {
-    String datapath = "";
-    if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
-        //内部存储
-        datapath = Environment.getExternalStorageDirectory().getAbsolutePath();
-    } else {
-        //获取U盘路径
-        String[] strArr = new String[] {
-            "/mnt/usb/", //mnt/usb/xxxx-xxxx
-            "/storage/usb/", //storage/usb/xxxx-xxxx
-            "/storage/", //storage/xxxx-xxxx
-            "/mnt/" //mnt/xxxx-xxxx
-        };
-        for (int i = 0; i < strArr.length; i++) {
-            if (datapath == null || datapath == "") {
-                File[] listFiles = new File(strArr[i]).listFiles();
-                if (listFiles != null && listFiles.length > 0) {
-                    for (File file: listFiles) {
-                        if (file.isDirectory() && file.getName().length() == 9 && file.getName().indexOf("-") == 4) {
-                            datapath = file.getAbsolutePath();
-                            break;
-                        }
-                    }
-                }
-            } else {
-                break;
-            }
-        }
-        if (datapath == null || datapath == "") {
-            //系统存储，需给予读写权限
-            datapath = "/data/local/tmp";
-        }
-    }
-    return datapath;
-}
     
     @Override
     public void start(int timeout, boolean daemon) throws IOException {
@@ -160,7 +124,7 @@ String getpath() {
                 } else if (fileName.startsWith("/file/")) {
                     try {
                         String f = fileName.substring(6);
-                        String root = getpath();
+                        String root = Environment.getExternalStorageDirectory().getAbsolutePath();
                         String file = root + "/" + f;
                         File localFile = new File(file);
                         if (localFile.exists()) {
@@ -221,7 +185,7 @@ String getpath() {
                                 String fn = params.get(k);
                                 String tmpFile = files.get(k);
                                 File tmp = new File(tmpFile);
-                                String root = getpath();
+                                String root = Environment.getExternalStorageDirectory().getAbsolutePath();
                                 File file = new File(root + "/" + path + "/" + fn);
                                 if (file.exists())
                                     file.delete();
@@ -240,7 +204,7 @@ String getpath() {
                     } else if (fileName.equals("/newFolder")) {
                         String path = params.get("path");
                         String name = params.get("name");
-                        String root = getpath();
+                        String root = Environment.getExternalStorageDirectory().getAbsolutePath();
                         File file = new File(root + "/" + path + "/" + name);
                         if (!file.exists()) {
                             file.mkdirs();
@@ -251,7 +215,7 @@ String getpath() {
                         return NanoHTTPD.newFixedLengthResponse(NanoHTTPD.Response.Status.OK, NanoHTTPD.MIME_PLAINTEXT, "OK");
                     } else if (fileName.equals("/delFolder")) {
                         String path = params.get("path");
-                        String root = getpath();
+                        String root = Environment.getExternalStorageDirectory().getAbsolutePath();
                         File file = new File(root + "/" + path);
                         if (file.exists()) {
                             FileUtils.recursiveDelete(file);
@@ -259,7 +223,7 @@ String getpath() {
                         return NanoHTTPD.newFixedLengthResponse(NanoHTTPD.Response.Status.OK, NanoHTTPD.MIME_PLAINTEXT, "OK");
                     } else if (fileName.equals("/delFile")) {
                         String path = params.get("path");
-                        String root = getpath();
+                        String root = Environment.getExternalStorageDirectory().getAbsolutePath();
                         File file = new File(root + "/" + path);
                         if (file.exists()) {
                             file.delete();
