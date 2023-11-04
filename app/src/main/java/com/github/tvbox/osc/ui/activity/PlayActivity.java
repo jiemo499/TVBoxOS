40fd6aaff60997279a6275dead6ed0b6d0e83f70
jiemo499 <113921400+jiemo499@users.noreply.github.com>
2023-11-04 14:46:34 +0000

    Update PlayActivity.java

--- app/src/main/java/com/github/tvbox/osc/ui/activity/PlayActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/PlayActivity.java
@@ -722,6 +722,7 @@ public class PlayActivity extends BaseActivity {
         }
         stopLoadWebView(true);
         stopParse();
+        Thunder.stop(); // 停止磁力下载
     }
 
     private VodInfo mVodInfo;
